9df84898f31e6e1c2a8f5b06a8b2deacc38f7c7d
Garai Márton <garaimarton.mail@gmail.com>
2023-07-29 00:38:46 +0200

    Change all Message classes to records.

--- CellCalculation/CreateMessage.cs
+++ CellCalculation/CreateMessage.cs
@@ -3,26 +3,7 @@
     using System;
     using BlazorReporter;
 
-    public class CreateMessage
+    public record CreateMessage(Action<int, int, bool> SetColor, int ParentX, int ParentY, int X, int Y, int ParentCount, IBlazorFeeder Logger)
     {
-
-        public CreateMessage(Action<int, int, bool> setColor, int parentX, int parentY, int x, int y, int parentCount, IBlazorFeeder logger)
-        {
-            SetColor = setColor;
-            ParentX = parentX;
-            ParentY = parentY;
-            ParentCount = parentCount;
-            X = x;
-            Y = y;
-            Logger = logger;
-        }
-
-        public Action<int, int, bool> SetColor { get; }
-        public int X { get; }
-        public int Y { get; }
-        public int ParentX { get; }
-        public int ParentY { get; }
-        public int ParentCount { get; }
-        public IBlazorFeeder Logger { get; }
     }
 }
--- CellCalculation/SetMockProperties.cs
+++ CellCalculation/SetMockProperties.cs
@@ -3,19 +3,7 @@
     using System.Collections.Generic;
     using Akka.Actor;
 
-    internal class SetMockProperties
+    internal record SetMockProperties(List<IActorRef> Children, IActorRef Parent, IActorRef[,] Neighbours, bool IsLiving = true)
     {
-        public SetMockProperties(List<IActorRef> children, IActorRef parent, IActorRef[,] neighbours, bool isLiving = true)
-        {
-            Children = children;
-            Parent = parent;
-            Neighbours = neighbours;
-            IsLiving = isLiving;
-        }
-
-        public IActorRef Parent { get; }
-        public IActorRef[,] Neighbours { get; }
-        public List<IActorRef> Children { get; }
-        public bool IsLiving { get; }
     }
 }
